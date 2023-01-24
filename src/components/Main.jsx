3eb330f82b75cc3f80565951a1b0776d714e9597
Aizhan Alymseitova <aizhan.ll86@gmail.com>
2023-01-24 08:36:27 -0800

    Typer added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,25 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import Footer from './Footer';
 import { useState } from 'react';
-import TypeWriterEffect from 'react-typewriter-effect';
 
-
+const TYPING_SPEED = 150;
+const DELETING_SPEED = 30;
 
 const Main = () => {
-  
+  const words = ['Blockchain', 'Smart contarcts', 'hello'];
+  const loopNum = 0;
+  const fullText = words[loopNum];
+
+  useEffect(() => {
+    words.leng
+  },[])
+  setTimeout
   return (
+    
     <div>
       <main  className='main-container'>
         <h2 className='main-container-text'>Web3 Solutions</h2>
-        <h1 className='main-container-dynamic-text'>
-        <TypeWriterEffect
-         textStyle={{
-          fontWeight: 500,
-          // fontSize: '1.5em',
-        }}
-        startDelay={1000}
-        cursorColor="#3F3D56"
-        multiText={[
-          'Blockcain',
-          'Smart contarcts'
-        ]}
-        multiTextDelay={100}
-        typeSpeed={200}
-      />
-          </h1>
+        <h1 className='main-container-dynamic-text'>{fullText}</h1>
       </main>
       <footer className='footer-main'>
         <Footer/>
